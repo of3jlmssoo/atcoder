be36db0d24c13946be2962dfb6f4677c198c25cb
of3jlmssoo <7a4wiy5vd638@gmail.com>
2021-12-25 22:38:49 +0900

    2021-12-25

--- 90/001/work.py
+++ 90/001/work.py
@@ -1,52 +1,33 @@
+lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 """
-分割する(短いピースに着目する)
-規定回数=規定回数+1
-分割後をlong_oneとshort_oneにセットする
-if 規定回数に達しているか？
-  return なんとかかんとか(短い方、long_one、short_one)
-long_oneで再帰コール(return will be p1, l1, s1)
-short_oneで再帰コール(return will be p2, l2, s2)
-(l1,s1)の短い方と(l2,s2)の短い方を比較して長い方を選択
 """
-tup_NL = (3, 34)
-tup_K = (1)
-tup_As = (8, 13, 26)
+low = 0
+high = len(lst) - 1
 
+mid = int(low + (high - low) / 2)
 
-def resolve():
-    N = tup_NL[0]
-    L = tup_NL[1]
-    K = tup_K
-    A = list(tup_As)
+target = 5
 
-    pieces = [0] + A + [L]
-    # print(pieces)
-    print(cut_jcake(L, K, pieces))
-
-
-def prepare_cut():
-    pass
-
-
-def cut_jcake(L, K, A):
-    """cut_jcake
-
-    Args:
-        L ([type]): ケーキの長さ
-        K ([type]): K回切る
-        A (list): 切る位置
-    """
-    print(L, K, A)
-    pieces = [A[p + 1] - A[p] for p in range(len(A) - 1)]
-    print(pieces)
-    mins = [i for i, j in enumerate(pieces) if j == min(pieces)]
-    print(mins)
-    result = 0
-    for m in mins:
-        if mins == 0:
+for i in range(len(lst)):
+    # print(i)
+    low = 0
+    high = len(lst) - 1
+    mid = int(low + (high - low) / 2)
+    target = i
+    # print(f'{target=} {mid=} {lst[target]=}')  # , end='')
+    while mid != lst[target]:
+        # print(f'{mid}')
+        if lst[target] < lst[mid]:
+            # print(f'then')
+            high = mid
+        elif lst[target] > lst[mid]:
+            # print(f'elif {lst[target]=} {lst[mid]=} {mid=} {target=} {lst[mid + 1]=} {lst[target]=}')
+            low = mid
+        if lst[mid + 1] == lst[target]:
+            # print(f'else')
+            mid = mid + 1
             pass
-        if mins == len(pieces) - 1:
-            pass
-
-
-resolve()
+            break
+        mid = int(low + (high - low) / 2)
+    print(f'{target=} {lst[mid]=} {mid=} ')
